251d0731dded8f644f1e4b69efeccd7d6e939032
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-05-22 13:04:04 +0000

    ex03-18

---
+++ ch03-Expression/ex03-18/errors.rkt
@@ -0,0 +1,30 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+; Var -> Void
+(define report-no-binding-found
+  (lambda (search-var)
+    (eopl:error 'apply-env ": No binding for ~s" search-var)
+  )
+)
+
+; Env -> Void
+(define report-invalid-env
+  (lambda (e)
+    (eopl:error 'apply-env ": Bad environment=~s" e)
+  )
+)
+
+; String x ExpVal -> Void
+(define report-expval-extractor-error
+  (lambda (name val)
+    (eopl:error name "invalid syntax: ~s" val)
+  )
+)
+
+(define report-invalid-scheme-val
+  (lambda (x)
+    (eopl:error 'any->expval "invalid value: ~s" x)
+  )
+)
---
+++ ch03-Expression/ex03-18/main.rkt
@@ -0,0 +1,20 @@
+#lang eopl
+
+(require "lang.rkt")
+(require "interp.rkt")
+(require "utils.rkt")
+
+; String -> ExpVal
+(define run
+  (lambda (string)
+    (value-of-program (scan&parse string))
+  )
+)
+
+(print
+  (run "
+    let u = 7
+    in unpack x y = cons (u, cons (3, emptylist))
+      in - (x, y)
+  ")
+) ; #(struct:num-val 4)
