eeb8f26b04619bd85bf2bbc59038b4e8998a118f
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-05-24 12:52:30 +0000

    ex03-22

---
+++ ch03-Expression/ex03-22/main.rkt
@@ -0,0 +1,22 @@
+#lang eopl
+
+(require "lang.rkt")
+(require "interp.rkt")
+(require "utils.rkt")
+
+; String -> ExpVal
+(define run
+  (lambda (string)
+    (value-of-program (scan&parse string))
+  )
+)
+
+(print
+  (run "
+    let x = 200
+      in let f = proc (z) -(z x)
+        in let x = 100
+          in let g = proc (z) -(z x)
+            in -((f 1) (g 1))
+  ")
+) ; #(struct:num-val -100)
