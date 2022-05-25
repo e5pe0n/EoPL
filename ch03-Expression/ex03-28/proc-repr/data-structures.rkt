682a189187898332f4a6e77acd707a7c2a283e06
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-05-25 12:24:23 +0000

    ex03-28 proc-repr

---
+++ ch03-Expression/ex03-28/proc-repr/data-structures.rkt
@@ -0,0 +1,89 @@
+#lang eopl
+
+(require "utils.rkt")
+(require "errors.rkt")
+(require "lang.rkt")
+
+(provide (all-defined-out))
+
+; Env = Var -> SchemeVal
+
+; () -> Env
+(define empty-env
+  (lambda ()
+    (lambda (search-var)
+      (report-no-binding-found search-var)
+    )
+  )
+)
+
+; Var * SchemeVal * Env -> Env
+(define extend-env
+  (lambda (saved-var saved-val saved-env)
+    (lambda (search-var)
+      (if (eqv? search-var saved-var)
+        saved-val
+        (apply-env saved-env search-var)
+      )
+    )
+  )
+)
+
+; Env * Var -> Env
+(define apply-env
+  (lambda (env search-var)
+    (env search-var)
+  )
+)
+
+; Proc = SchemeVal -> SchemeVal
+
+; SchemeVal -> Bool
+(define proc?
+  (lambda (val)
+    (procedure? val)
+  )
+)
+
+
+(define-datatype expval expval?
+  (num-val
+    (num number?)
+  )
+  (bool-val
+    (bool boolean?)
+  )
+  (proc-val
+    (proc1 proc?)
+  )
+)
+
+; ExpVal -> Number
+(define expval->num
+  (lambda (val)
+    (cases expval val
+      (num-val (num) num)
+      (else report-expval-extractor-error 'num val)
+    )
+  )
+)
+
+; ExpVal -> Bool
+(define expval->bool
+  (lambda (val)
+    (cases expval val
+      (bool-val (bool) bool)
+      (else report-expval-extractor-error 'bool val)
+    )
+  )
+)
+
+; ExpVal -> Proc
+(define expval->proc
+  (lambda (val)
+    (cases expval val
+      (proc-val (proc1) proc1)
+      (else report-expval-extractor-error 'proc val)
+    )
+  )
+)
---
+++ ch03-Expression/ex03-28/proc-repr/interp.rkt
@@ -0,0 +1,90 @@
+#lang eopl
+
+(require "data-structures.rkt")
+(require "lang.rkt")
+
+(provide (all-defined-out))
+
+; () -> Env
+(define init-env
+  (lambda ()
+    (extend-env 'i (num-val 1)
+      (extend-env 'v (num-val 5)
+        (extend-env 'x (num-val 10)
+          (empty-env)
+        )
+      )
+    )
+  )
+)
+
+; Program -> ExpVal
+(define value-of-program
+  (lambda (pgm)
+    (cases program pgm
+      (a-program (exp1)
+        (value-of exp1 (init-env))
+      )
+    )
+  )
+)
+
+; Exp x Env -> ExpVal
+(define value-of
+  (lambda (exp env)
+    (cases expression exp
+      (const-exp (num) (num-val num))
+      (var-exp (var) (apply-env env var))
+      (diff-exp (exp1 exp2)
+        (num-val
+          (-
+            (expval->num (value-of exp1 env))
+            (expval->num (value-of exp2 env))
+          )
+        )
+      )
+      (zero?-exp (exp1)
+        (bool-val (zero? (expval->num (value-of exp1 env))))
+      )
+      (if-exp (exp1 exp2 exp3)
+        (if (expval->bool (value-of exp1 env))
+          (value-of exp2 env)
+          (value-of exp3 env)
+        )
+      )
+      (let-exp (var exp1 body)
+        (value-of body
+          (extend-env var (value-of exp1 env) env)
+        )
+      )
+      (proc-exp (var body)
+        (proc-val (procedure var body))
+      )
+      (call-exp (rator rand)
+        (let
+          (
+            [proc1 (expval->proc (value-of rator env))]
+            [arg (value-of rand env)]
+          )
+          (apply-procedure proc1 arg env)
+        )
+      )
+    )
+  )
+)
+
+; Var * Exp * Env -> Proc
+(define procedure
+  (lambda (var body)
+    (lambda (val env)
+      (value-of body (extend-env var val env))
+    )
+  )
+)
+
+; Proc * SchemeVal -> SchemeVal
+(define apply-procedure
+  (lambda (proc1 val env)
+    (proc1 val env)
+  )
+)
