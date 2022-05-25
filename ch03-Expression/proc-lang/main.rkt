7ecc180a9d855f4720c7bcc3d1eeb64444cb6cf3
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-05-25 01:24:21 +0000

    ex03-26

--- ch03-Expression/proc-lang/main.rkt
+++ ch03-Expression/proc-lang/main.rkt
@@ -14,9 +14,9 @@
 (print
   (run "
     let x = 200
-      in let f = proc (z) -(z, x)
-        in let x = 100
-          in let g = proc (z) -(z, x)
-            in -((f 1), (g 1))
+    in let f = proc (z) -(z, x)
+      in let x = 100
+        in let g = proc (z) -(z, x)
+          in -((f 1), (g 1))
   ")
 ) ; #(struct:num-val -100)
