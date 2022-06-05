21ca4d4246a6957ad920607572c6756fa9082eb9
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-06-05 03:20:03 +0000

    ex03-36

---
+++ ch03-Expression/ex03-36/main.rkt
@@ -0,0 +1,21 @@
+#lang eopl
+
+(require "lang.rkt")
+(require "interp.rkt")
+(require "utils.rkt")
+
+; String -> ExpVal
+(define run
+  (lambda (string)
+    (value-of-program (scan&parse string))
+  )
+)
+
+(print
+  (run "
+    letrec
+      even (x) = if zero? (x) then 1 else (odd -(x, 1))
+      odd (x) = if zero? (x) then 0 else (even -(x, 1))
+    in (odd 13)
+  ")
+) ; #(struct:num-val 1)
