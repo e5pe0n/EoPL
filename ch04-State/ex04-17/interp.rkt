29488cf8c935f564a00ed4ed94d1caec93a51e12
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-06-21 12:16:30 +0000

    ex04-17

---
+++ ch04-State/ex04-17/interp.rkt
@@ -0,0 +1,121 @@
+#lang eopl
+
+(require "data-structures.rkt")
+(require "lang.rkt")
+(require "utils.rkt")
+
+(provide (all-defined-out))
+
+; Program -> ExpVal
+(define value-of-program
+  (lambda (pgm)
+    (initialize-store!)
+    (cases program pgm
+      (a-program (exp1)
+        (value-of exp1 (init-env))
+      )
+    )
+  )
+)
+
+; Exp x Env -> ExpVal
+(define value-of
+  (lambda (exp env)
+    (cases expression exp
+      (const-exp (num) (num-val num))
+      (var-exp (var) (deref (apply-env env var)))
+      (diff-exp (exp1 exp2)
+        (num-val
+          (-
+            (expval->num (value-of exp1 env))
+            (expval->num (value-of exp2 env))
+          )
+        )
+      )
+      (zero?-exp (exp1)
+        (bool-val (zero? (expval->num (value-of exp1 env))))
+      )
+      (if-exp (exp1 exp2 exp3)
+        (if (expval->bool (value-of exp1 env))
+          (value-of exp2 env)
+          (value-of exp3 env)
+        )
+      )
+      (let-exp (vars exps body)
+        (value-of body
+          (let f ([vs vars] [es exps])
+            (if (null? vs)
+              env
+              (extend-env (car vs) (newref (value-of (car es) env)) (f (cdr vs) (cdr es)))
+            )
+          )
+        )
+      )
+      (letrec-exp (p-names b-vars-list p-bodies letrec-body)
+        (value-of
+          letrec-body
+          (extend-env-rec p-names b-vars-list p-bodies env)
+        )
+      )
+      (proc-exp (vars body)
+        (proc-val (procedure vars body env))
+      )
+      (call-exp (rator rands)
+        (let
+          (
+            [proc1 (expval->proc (value-of rator env))]
+            [args (map (lambda (x) (value-of x env)) rands)]
+          )
+          (apply-procedure proc1 args)
+        )
+      )
+      (newref-exp (exp1)
+        (ref-val (newref (value-of exp1 env)))
+      )
+      (deref-exp (exp1)
+        (deref (expval->ref (value-of exp1 env)))
+      )
+      (setref-exp (exp1 exp2)
+        (begin
+          (setref!
+            (expval->ref (value-of exp1 env))
+            (value-of exp2 env)
+          )
+          (num-val 23)
+        )
+      )
+      (assign-exp (var exp1)
+        (begin
+          (setref!
+            (apply-env env var)
+            (value-of exp1 env)
+          )
+          (num-val 27)
+        )
+      )
+      (begin-exp (exp1 exps)
+        (let ([vals (map (lambda (x) (value-of x env)) (cons exp1 exps))])
+          (list-ref vals (- (length vals) 1))
+        )
+      )
+    )
+  )
+)
+
+; Proc * ExpVal -> ExpVal
+(define apply-procedure
+  (lambda (proc1 vals)
+    (cases proc proc1
+      (procedure (vars body saved-env)
+        (value-of body
+          (let f ([vrs vars] [vls vals])
+            (if (null? vrs)
+              saved-env
+              (extend-env (car vrs) (newref (car vls)) (f (cdr vrs) (cdr vls)))
+            )
+          )
+        )
+      )
+    )
+  )
+)
--- ch04-State/ex04-17/main.rkt
+++ ch04-State/ex04-17/main.rkt
@@ -16,10 +16,18 @@
     let f = proc (x, y) -(x, -(0, y))
     in (f 3 4)
   ")
-) ; #(struct:num-val 7)
+) ; #(struct:num-val 12)
 (print
   (run "
     let f = proc () -(1, -2)
     in (f)
   ")
 ) ; #(struct:num-val 3)
+(print
+  (run "
+    letrec
+      even (x) = if zero? (x) then 1 else (odd -(x, 1))
+      odd (x) = if zero? (x) then 0 else (even -(x, 1))
+    in (odd 13)
+  ")
+) ; #(struct:num-val 1)
