a69484769327841d7bdbaeeb239541770567e3f7
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-09-27 13:39:28 +0000

    refactor let3

--- ch05-Continuation-Passing_Interpreters/ex05-04/main.rkt
+++ ch05-Continuation-Passing_Interpreters/ex05-04/main.rkt
@@ -18,17 +18,8 @@
 ; #(struct:num-val 30)
 
 (eopl:printf "~s~%" (run "
-  let3 a = 44, b = 11, c = 12
+  let3 a = 44 b = 11 c = 12
   in -(-(a, b), c)
 "))
 ; End of computation.
 ; #(struct:num-val 33)
-
-(eopl:printf "~s~%" (run "
-  let3 p1 = proc (x) -(x, 1),
-        p2 = proc (x) -((p1 x), 1),
-        p3 = proc (x) -((p2 x), 1)
-  in (p3 10)
-"))
-; End of computation.
-; #(struct:num-val 7)
