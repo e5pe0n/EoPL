78fa9fecb2bba4ab87ee08395f7aa5d5de0b6a79
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-11-05 16:30:03 +0000

    ex05-18

---
+++ ch05-Continuation-Passing_Interpreters/ex05-18/main.rkt
@@ -0,0 +1,49 @@
+#lang eopl
+
+(require "lang.rkt")
+(require "interp.rkt")
+(require "utils.rkt")
+
+; String -> ExpVal
+(define run
+  (lambda (string)
+    (value-of-program (scan&parse string))
+  )
+)
+
+; (println (run "
+;   let a = 44
+;       b = 11
+;       c = 12
+;   in -(-(a, b), c)
+; "))
+; ; End of Computation.
+; ; #(struct:num-val 21)
+
+; (println (run "
+;   let p1 = proc (x) -(x, 1)
+;       p2 = proc (p) (p 10)
+;   in (p2 p1)
+; "))
+; ; End of Computation.
+; ; #(struct:num-val 9)
+
+; (println (run "
+;   let p1 = proc (x, y) -(x, y)
+;   in (p1 10 1)
+; "))
+; ; End of Computation.
+; ; #(struct:num-val 9)
+
+; (println (run "
+;   let p1 = proc (x, y, z) -(-(x, y), z)
+;   in (p1 10 1 2)
+; "))
+; ; End of Computation.
+; ; #(struct:num-val 7)
+
+(println (run "
+  let p1 = proc (x, y) -(x, y)
+  in let p2 = proc (x) -((p1 10 7), x)
+  in (p2 1)
+"))
---
+++ ch05-Continuation-Passing_Interpreters/ex05-18/utils.rkt
@@ -0,0 +1,24 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+; Any -> Bool
+(define s-val?
+  (lambda (x)
+    #t
+  )
+)
+
+; SchemeVal -> Bool
+(define identifier?
+  (lambda (x)
+    (symbol? x)
+  )
+)
+
+; SchemeVal -> ()
+(define println
+  (lambda (x)
+    (eopl:printf "~s~%" x)
+  )
+)
