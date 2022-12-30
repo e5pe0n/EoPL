844c47b85236a4b506a7543cef8ac1e9c58e9f0f
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-12-30 11:21:12 +0000

    ex05-43

---
+++ ch05-Continuation-Passing_Interpreters/ex05-43/data-structures.rkt
@@ -0,0 +1,148 @@
+#lang eopl
+
+(require "utils.rkt")
+(require "lang.rkt")
+(require "errors.rkt")
+
+(provide (all-defined-out))
+
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env
+    (saved-var symbol?)
+    (saved-val s-val?)
+    (saved-env environment?)
+  )
+  (extend-env-rec
+    (p-name identifier?)
+    (b-vars (list-of identifier?))
+    (body expression?)
+    (env environment?)
+  )
+)
+
+; () -> Env
+(define init-env
+  (lambda ()
+    (extend-env 'i (num-val 1)
+      (extend-env 'v (num-val 5)
+        (extend-env 'x (num-val 10)
+          (empty-env)
+        )
+      )
+    )
+  )
+)
+
+; Env * Var -> ExpVal
+(define apply-env
+  (lambda (env search-var)
+    (cases environment env
+      (empty-env () (report-no-binding-found search-var))
+      (extend-env (saved-var saved-val saved-env)
+        (if (eqv? search-var saved-var)
+          saved-val
+          (apply-env saved-env search-var)
+        )
+      )
+      (extend-env-rec (p-name b-vars p-body saved-env)
+        (if (eqv? p-name search-var)
+          (proc-val (procedure b-vars p-body env))
+          (apply-env saved-env search-var)
+        )
+      )
+    )
+  )
+)
+
+(define-datatype proc proc?
+  (procedure
+    (vars (list-of identifier?))
+    (body expression?)
+    (saved-env environment?)
+  )
+  (procedure-cont
+    (saved-cont (lambda (x) (and (procedure? (car x)) (procedure? (cdr x)))))
+  )
+)
+
+(define-datatype expval expval?
+  (num-val
+    (num number?)
+  )
+  (bool-val
+    (bool boolean?)
+  )
+  (proc-val
+    (proc1 proc?)
+  )
+  (list-val
+    (list1 list?)
+  )
+)
+
+; ExpVal -> Number
+(define expval->num
+  (lambda (val)
+    (cases expval val
+      (num-val (num) num)
+      (else report-expval-extractor-error 'num val)
+    )
+  )
+)
+
+; ExpVal -> Bool
+(define expval->bool
+  (lambda (val)
+    (cases expval val
+      (bool-val (bool) bool)
+      (else report-expval-extractor-error 'bool val)
+    )
+  )
+)
+
+; ExpVal -> Proc
+(define expval->proc
+  (lambda (val)
+    (cases expval val
+      (proc-val (proc1) proc1)
+      (else report-expval-extractor-error 'proc val)
+    )
+  )
+)
+
+; ExpVal -> List
+(define expval->list
+  (lambda (val)
+    (cases expval val
+      (list-val (list1) list1)
+      (else report-expval-extractor-error 'list val)
+    )
+  )
+)
+
+; ExpVal -> SchemeVal
+(define expval->any
+  (lambda (val)
+    (cases expval val
+      (num-val (num) num)
+      (bool-val (bool) bool)
+      (proc-val (proc1) proc1)
+      (list-val (list1) list1)
+      (else report-expval-extractor-error 'list val)
+    )
+  )
+)
+
+; SchemeVal -> ExpVal
+(define any->expval
+  (lambda (v)
+    (cond
+      ((number? v) (num-val v))
+      ((boolean? v) (bool-val v))
+      ((proc? v) (proc-val v))
+      ((list? v) (list-val v))
+      (else report-invalid-scheme-value-error v)
+    )
+  )
+)
