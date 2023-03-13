db85bdfda34a31df8301fb32d8397c406c24d441
e5pe0n <1izard.synchr0fi5h@gmail.com>
2023-03-13 07:51:29 +0000

    ex06-15

---
+++ ch06-Continuation-Passing_Style/ex06-15/interp.rkt
@@ -0,0 +1,170 @@
+#lang eopl
+
+(require "utils.rkt")
+(require "data-structures.rkt")
+(require "lang.rkt")
+(require "errors.rkt")
+
+(provide (all-defined-out))
+
+; FinalAnswer = ExpVal
+; Program -> FinalAnswer
+(define value-of-program
+  (lambda (pgm)
+    (cases program pgm
+      (a-program (exp1)
+        (value-of/k exp1 (init-env))
+      )
+    )
+  )
+)
+
+
+; TfExp * Env -> FinalAnswer
+(define value-of/k
+  (lambda (exp env)
+    (cases tf-expression exp
+      (simple-exp->exp (simple)
+        (value-of-simple-exp simple env)
+      )
+      (cps-let-exp (vars rhss body)
+        (value-of/k body
+          (extend-env* vars (map (lambda (rhs) (value-of-simple-exp rhs env)) rhss) env)
+        )
+      )
+      (cps-letrec-exp (p-names b-varss p-bodies letrec-body)
+        (value-of/k letrec-body
+          (extend-env-rec* p-names b-varss p-bodies env)
+        )
+      )
+      (cps-if-exp (simple1 body1 body2)
+        (if (cases expval (value-of-simple-exp simple1 env)
+              (num-val (num) (not (= num 0)))
+              (bool-val (bool) bool)
+              (proc-val (proc1) #t)
+              (list-val (list1) (not (null? list1)))
+            )
+          (value-of/k body1 env)
+          (value-of/k body2 env)
+        )
+      )
+      (cps-call-exp (rator rands)
+        (let
+          (
+            [rator-proc (expval->proc (value-of-simple-exp rator env))]
+            [rand-vals (map (lambda (simple) (value-of-simple-exp simple env)) rands)]
+          )
+          (apply-procedure/k rator-proc rand-vals)
+        )
+      )
+    )
+  )
+)
+
+; SimpleExp * Env -> FinalAnswer
+(define value-of-simple-exp
+  (lambda (exp env)
+    (cases simple-expression exp
+      (const-exp (num) (num-val num))
+      (var-exp (var) (apply-env env var))
+      (cps-diff-exp (simple1 simple2)
+        (num-val
+          (-
+            (expval->num (value-of-simple-exp simple1 env))
+            (expval->num (value-of-simple-exp simple2 env))
+          )
+        )
+      )
+      (cps-mul-exp (simple1 simple2)
+        (num-val
+          (*
+            (expval->num (value-of-simple-exp simple1 env))
+            (expval->num (value-of-simple-exp simple2 env))
+          )
+        )
+      )
+      (cps-add1-exp (simple)
+        (num-val
+          (+ (expval->num (value-of-simple-exp simple env)) 1)
+        )
+      )
+      (cps-zero?-exp (simple)
+        (bool-val (zero? (expval->num (value-of-simple-exp simple env))))
+      )
+      (cps-number?-exp (simple)
+        (bool-val
+          (cases expval (value-of-simple-exp simple env)
+            (num-val (num) #t)
+            (else #f)
+          )
+        )
+      )
+      (cps-equal?-exp (simple1 simple2)
+        (bool-val
+          (=
+            (expval->num (value-of-simple-exp simple1 env))
+            (expval->num (value-of-simple-exp simple2 env))
+          )
+        )
+      )
+      (cps-less?-exp (simple1 simple2)
+        (bool-val
+          (<
+            (expval->num (value-of-simple-exp simple1 env))
+            (expval->num (value-of-simple-exp simple2 env))
+          )
+        )
+      )
+      (cps-greater?-exp (simple1 simple2)
+        (bool-val
+          (>
+            (expval->num (value-of-simple-exp simple1 env))
+            (expval->num (value-of-simple-exp simple2 env))
+          )
+        )
+      )
+      (cps-null?-exp (simple)
+        (bool-val (null? (expval->list (value-of-simple-exp simple env))))
+      )
+      (cps-proc-exp (vars tf-exp)
+        (proc-val (procedure vars tf-exp env))
+      )
+      (cps-emptylist-exp () (list-val '()))
+      (cps-list-exp (simples)
+        (list-val
+          (map
+            (lambda (simple)
+              (expval->any (value-of-simple-exp simple env))
+            )
+            simples
+          )
+        )
+      )
+      (cps-car-exp (simple)
+        (any->expval (car (expval->list (value-of-simple-exp simple env))))
+      )
+      (cps-cdr-exp (simple)
+        (list-val (cdr (expval->list (value-of-simple-exp simple env))))
+      )
+      (cps-cons-exp (simple1 simple2)
+        (list-val
+          (cons
+            (expval->any (value-of-simple-exp simple1 env))
+            (expval->list (value-of-simple-exp simple2 env))
+          )
+        )
+      )
+    )
+  )
+)
+
+; Proc * ExpVal -> FinalAnswer
+(define apply-procedure/k
+  (lambda (proc1 vals)
+    (cases proc proc1
+      (procedure (vars body saved-env)
+        (value-of/k body (extend-env* vars vals saved-env))
+      )
+    )
+  )
+)
---
+++ ch06-Continuation-Passing_Style/ex06-15/utils.rkt
@@ -0,0 +1,53 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+; Any -> Bool
+(define s-val?
+  (lambda (x)
+    #t
+  )
+)
+
+; SchemeVal -> Bool
+(define identifier?
+  (lambda (x)
+    (symbol? x)
+  )
+)
+
+; SchemeVal -> ()
+(define println
+  (lambda (x)
+    (eopl:printf "~s~%" x)
+  )
+)
+
+; Listof(T) * ((T) -> Bool) -> Listof(T)
+(define filter
+  (lambda (lst pred)
+    (let f ([lst1 lst])
+      (if (null? lst1)
+        lst1
+        (let ([head (car lst1)] [tail (cdr lst1)])
+          (if (pred head)
+            (cons head (f tail))
+            (f tail)
+          )
+        )
+      )
+    )
+  )
+)
+
+; List -> List
+(define copy-list
+  (lambda (lst)
+    (let f ([lst1 lst])
+      (if (null? lst1)
+        lst1
+        (cons (car lst1) (f (cdr lst1)))
+      )
+    )
+  )
+)
