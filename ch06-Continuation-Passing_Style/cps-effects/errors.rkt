7343b9e13d4d9aabbb73ee4c7ada791860a1344f
e5pe0n <1izard.synchr0fi5h@gmail.com>
2023-03-26 12:34:21 +0000

    cps-effects

---
+++ ch06-Continuation-Passing_Style/cps-effects/errors.rkt
@@ -0,0 +1,52 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+; Var -> ()
+(define report-no-binding-found
+  (lambda (search-var)
+    (eopl:error 'no-binding-error ": No binding for ~s" search-var)
+  )
+)
+
+; Env -> ()
+(define report-invalid-env
+  (lambda (e)
+    (eopl:error 'invalid-env-error ": Bad environment=~s" e)
+  )
+)
+
+; String * ExpVal -> ()
+(define report-expval-extractor-error
+  (lambda (name val)
+    (eopl:error name "invalid syntax: ~s" val)
+  )
+)
+
+; SchemeVal -> ()
+(define report-invalid-scheme-value-error
+  (lambda (v)
+    (eopl:error 'invalid-scheme-error "invalid scheme value: ~s" v)
+  )
+)
+
+; () -> ()
+(define report-uncaught-exception
+  (lambda ()
+    (eopl:error 'uncaught-exception-error "no excpetion handling")
+  )
+)
+
+; InpExp -> ()
+(define report-invalid-exp-to-cps-of-simple-exp
+  (lambda (exp)
+    (eopl:error 'invalid-exp-to-cps-of-simple-exp "invalid exp: ~s" exp)
+  )
+)
+
+; Ref * Sto -> ()
+(define report-invalid-reference
+  (lambda (ref store)
+    (eopl:error 'invalid-ref "invalid reference: ref=~s, store=~s" ref store)
+  )
+)
