c0da311e8561a40dbdbec779aaae14dafb177a1f
e5pe0n <1izard.synchr0fi5h@gmail.com>
2023-04-29 04:24:16 +0000

    fix inferred

--- ch07-Types/inferred/type-checker.rkt
+++ ch07-Types/inferred/type-checker.rkt
@@ -6,8 +6,8 @@
 
 (define type-check!
   (lambda (string)
-    (type-to-external-form
-      (type-of-program (scan&parse string))
+    (let ([ty (type-to-external-form (type-of-program (scan&parse string)))])
+      (apply-subst-to-sexp (canonical-subst ty) ty)
     )
   )
 )
@@ -38,3 +38,22 @@
   in foo
 "))
 ; (int -> int)
+
+(println (type-check! "
+  letrec ? even(odd: ?) = proc(x: ?) if zero?(x) then 1 else (odd -(x, 1))
+  in letrec ? odd(x: ?) = if zero?(x) then 0 else ((even odd) -(x, 1))
+  in (odd 13)
+"))
+; int
+
+(println (type-check! "
+  letrec ? f(x: ?) = (f x)
+  in f
+"))
+; (tvar0 -> tvar1)
+
+(println (type-check! "
+  letrec ? f(x: ?) = (f x)
+  in proc(n: ?) (f -(n, 1))
+"))
+; (int -> tvar0)
