512484570f201967d93426171b530758df434e97
e5pe0n <1izard.synchr0fi5h@gmail.com>
2023-04-30 04:48:38 +0000

    ex07-23

---
+++ ch07-Types/ex07-23/data-structures.rkt
@@ -0,0 +1,107 @@
+#lang eopl
+
+(require "utils.rkt")
+(require "lang.rkt")
+
+(provide (all-defined-out))
+
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env
+    (saved-var symbol?)
+    (saved-val s-val?)
+    (saved-env environment?)
+  )
+  (extend-env-rec
+    (p-name identifier?)
+    (b-var identifier?)
+    (body expression?)
+    (env environment?)
+  )
+)
+
+; () -> Env
+(define init-env
+  (lambda ()
+    (empty-env)
+  )
+)
+
+; Env * Var -> ExpVal
+(define apply-env
+  (lambda (env search-var)
+    (cases environment env
+      (empty-env () (report-no-binding-found 'apply-env search-var))
+      (extend-env (saved-var saved-val saved-env)
+        (if (eqv? search-var saved-var)
+          saved-val
+          (apply-env saved-env search-var)
+        )
+      )
+      (extend-env-rec (p-name b-var p-body saved-env)
+        (if (eqv? p-name search-var)
+          (proc-val (procedure b-var p-body env))
+          (apply-env saved-env search-var)
+        )
+      )
+    )
+  )
+)
+
+(define-datatype proc proc?
+  (procedure
+    (var identifier?)
+    (body expression?)
+    (saved-env environment?)
+  )
+)
+
+(define-datatype expval expval?
+  (num-val
+    (num number?)
+  )
+  (bool-val
+    (bool boolean?)
+  )
+  (proc-val
+    (proc1 proc?)
+  )
+)
+
+; ExpVal -> Number
+(define expval->num
+  (lambda (val)
+    (cases expval val
+      (num-val (num) num)
+      (else report-expval-extractor-error 'expval->num val)
+    )
+  )
+)
+
+; ExpVal -> Bool
+(define expval->bool
+  (lambda (val)
+    (cases expval val
+      (bool-val (bool) bool)
+      (else report-expval-extractor-error 'expval->bool val)
+    )
+  )
+)
+
+; ExpVal -> Proc
+(define expval->proc
+  (lambda (val)
+    (cases expval val
+      (proc-val (proc1) proc1)
+      (else report-expval-extractor-error 'expval->proc val)
+    )
+  )
+)
+
+; String * ExpVal -> ()
+(define report-expval-extractor-error
+  (lambda (err-name val)
+    (eopl:error err-name "Couldn't extract inside value; val=~s" val)
+  )
+)
+
