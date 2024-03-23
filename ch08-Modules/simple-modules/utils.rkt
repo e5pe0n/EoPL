e47e9f2731ee2cfb2495c4105154bb62894b3415
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-03-23 22:48:48 +0900

    ch08: add utils

--- ch08-Modules/simple-modules/utils.rkt
+++ ch08-Modules/simple-modules/utils.rkt
@@ -29,3 +29,93 @@
     (eopl:error err-name ": No binding for ~s" search-var)
   )
 )
+
+; List * Sym -> List
+(define join
+  (lambda (xs sep)
+    (if (null? xs)
+      xs
+      (let loop ([xs xs])
+        (if (= (length xs) 1)
+          xs
+          (cons (car xs) (cons sep (loop (cdr xs))))
+        )
+      )
+    )
+  )
+)
+
+; ((T2 * T1) -> T2) * Listof(T1) * T2 -> T2
+(define reduce
+  (lambda (fn xs init-val)
+    (if (null? xs)
+      init-val
+      (reduce fn (cdr xs) (fn init-val (car xs)))
+    )
+  )
+)
+
+; Listof(List) -> Listof(List)
+(define zip
+  (lambda (xss)
+    (if (or (null? xss) (any null? xss))
+      '()
+      (cons (map car xss) (zip (map cdr xss)))
+    )
+  )
+)
+
+; (T1 -> Bool) * Listof(T1) -> Bool
+(define all
+  (lambda (pred xs)
+    (if (null? xs)
+      #t
+      (let ([b (pred (car xs))])
+        (if b (all pred (cdr xs)) #f)
+      )
+    )
+  )
+)
+
+; (T1 -> Bool) * Listof(T1) -> Bool
+(define any
+  (lambda (pred xs)
+    (if (null? xs)
+      #f
+      (let ([b (pred (car xs))])
+        (if b #t (any pred (cdr xs)))
+      )
+    )
+  )
+)
+
+; (SchemeVal -> Bool) -> (List -> Bool)
+(define list-of
+  (lambda (pred)
+    (lambda (list1)
+      (if (null? list1)
+        #t
+        (and
+          (pred (car list1))
+          (list-of pred) (cdr list1)
+        )
+      )
+    )
+  )
+)
+
+; (T -> Bool) * Listof(T) -> T | #f
+(define position
+  (lambda (pred xs)
+    (let f ([xs xs] [i 0])
+      (if (null? xs)
+        #f
+        (if (pred (car xs))
+          i
+          (f (cdr xs) (+ i 1))
+        )
+      )
+    )
+  )
+)
+
