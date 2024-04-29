ad3dbb769d48b55012089c35a93a70e2065ba90d
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-04-29 23:25:55 +0900

    --amend

--- ch09-Objects_and_Classes/ex09-01/main.rkt
+++ ch09-Objects_and_Classes/ex09-01/main.rkt
@@ -13,14 +13,33 @@
 )
 
 (println (run "
+  class counter extends object
+    field cnt
+    method initialize()
+      set cnt = 0
+    method incr()
+      set cnt = +(cnt, 1)
+    method cnt()
+      cnt
+
   class queue extends object
     field xs
-    method initialize()
-      set xs = list()
+    field num-ops
+    field cnter
+    method initialize(new-cnter)
+      begin
+        set xs = list();
+        set num-ops = 0;
+        set cnter = new-cnter
+      end
     method empty?()
       null?(xs)
     method enqueue(x)
-      set xs = cons(x, xs)
+      begin
+        set xs = cons(x, xs);
+        set num-ops = +(num-ops, 1);
+        send cnter incr()
+      end
     method dequeue()
       let head = 0
       in letrec loop(ys) = if null?(cdr(ys))
@@ -31,26 +50,33 @@
                         else cons(car(ys), (loop cdr(ys)))
       in begin
         set xs = (loop xs);
+        set num-ops = +(num-ops, 1);
+        send cnter incr();
         head
       end
+    method num-ops()
+      num-ops
 
-  let q = new queue()
+  let cnter = new counter()
+  in let q1 = new queue(cnter)
+  in let q2 = new queue(cnter)
   in begin
-    print(send q empty?());
-    send q enqueue(10);
-    print(send q empty?());
-    send q enqueue(20);
-    send q enqueue(30);
-    print(send q dequeue());
-    print(send q dequeue());
-    print(send q dequeue());
-    print(send q empty?())
+    send q1 enqueue(10);
+    send q1 enqueue(20);
+    send q1 enqueue(30);
+    send q1 dequeue();
+    send q1 dequeue();
+    send q1 dequeue();
+    print(send q1 num-ops());
+
+    send q2 enqueue(100);
+    send q2 enqueue(200);
+    print(send q2 num-ops());
+
+    print(send cnter cnt())
   end
 "))
-; #(struct:bool-val #t)
-; #(struct:bool-val #f)
-; #(struct:num-val 10)
-; #(struct:num-val 20)
-; #(struct:num-val 30)
-; #(struct:bool-val #t)
+; #(struct:num-val 6)
+; #(struct:num-val 2)
+; #(struct:num-val 8)
 ; 29
